55627b2beda711313b1cc3b5d2730f82a7094da6
Sarang Yang <sarang628@naver.com>
2022-08-27 15:09:20 +0900

    update

--- library/src/main/java/com/sarang/feed_detail/ui/usecase/ItemWriteCommentUsecase.kt
+++ library/src/main/java/com/sarang/feed_detail/ui/usecase/ItemWriteCommentUsecase.kt
@@ -1,6 +1,10 @@
 package com.sarang.feed_detail.ui.usecase
 
+import android.view.View
+import kotlinx.coroutines.flow.MutableStateFlow
+
 data class ItemWriteCommentUsecase(
     val profilePictureUrl : String,
-    var comment : String
+    var comment : MutableStateFlow<String>,
+    val clickPost : View.OnClickListener
 )
